5af343b9e3adf7f057c4183afd23f5ca01d771a6
Chi-Shen Wu <s97403002a@gmail.com>
2020-08-27 19:06:22 +0800

    Django & the Python Shell

--- emails/models.py
+++ emails/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class EmailEntry(models.Model):
     # 定義null = True會將你的資料庫相應欄位設置為NULL（不定義就會設置為NOT NULL）。對於Django的欄位類型（如DateTimeField、ForeignKey），空值意味著會在資料庫中存儲NULL值
-    #定義blank=True，這取決於該欄位在表單中是否為必填項。表單包括admin管理以及自己定義的表單。如果定義了blank=True，表單中相應的欄位可以不填寫內容
+    # 定義blank=True，這取決於該欄位在表單中是否為必填項。表單包括admin管理以及自己定義的表單。如果定義了blank=True，表單中相應的欄位可以不填寫內容
     email = models.EmailField()
     name = models.CharField(max_length=120, blank=True)
     bio = models.TextField(blank=True)
